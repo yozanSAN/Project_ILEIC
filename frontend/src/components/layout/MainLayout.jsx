58995aa875a6bed7fb40139051a932411ed37754
ayoub <ayoubpro183@gmail.com>
2026-03-01 02:18:18 +0000

    feat: add secretary UI components +  update MainLayout to support children in StagiaireDashboard

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -3,11 +3,14 @@ import Navbar from "./Navbar"
 import { secretaryItems } from "../../data/secretary/secretary_sidebar_items"
 import user from '../../data/secretary/user'
 
-export default function MainLayout() {
+export default function MainLayout({ children }) {
   return (
     <div>
-      <Sidebar items={secretaryItems}/>
-      <Navbar user={user}/>
+      <Sidebar items={secretaryItems} />
+      <Navbar user={user} />
+      <main className="flex-1 overflow-x-hidden overflow-y-auto p-6">
+        {children}
+      </main>
     </div>
   )
 }
