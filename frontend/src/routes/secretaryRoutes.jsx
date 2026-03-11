7e2c49122a433db1809d40c9cd8722c3e5e7b790
ayoub <ayoubpro183@gmail.com>
2026-03-11 02:03:58 +0000

    ''

--- frontend/src/routes/secretaryRoutes.jsx
+++ frontend/src/routes/secretaryRoutes.jsx
@@ -1,6 +1,6 @@
 import StagiaireDashboard from "../pages/secretary/Stagiaires/StagiaireDashboard";
 import AjouterStagiaire from "../pages/secretary/Stagiaires/AjouterStagiaire";
-import Notes from './../pages/stagiaire/Notes';
+import Notes from '../pages/secretary/Notes';
 
 const secretaryRoutes = [
     { path: "/secretaire/stagiaire", element: <StagiaireDashboard /> },
