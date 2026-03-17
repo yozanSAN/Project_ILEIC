f447027c934105046655e5a6ee0c17b6185d3a87
ayoub <ayoubpro183@gmail.com>
2026-03-17 23:39:58 +0000

    feat: implement document management for stagiaires with selection and display components

--- frontend/src/data/secretary/secretary_sidebar_items.js
+++ frontend/src/data/secretary/secretary_sidebar_items.js
@@ -11,7 +11,9 @@ export const secretaryItems = [
   { label: "Stagiaires", path: "/secretaire/stagiaire", icon: Users }, 
   { label: "Contrôles", path: "/secretaire/controles", icon: FileText },
   { label: "Cours", path:  "/secretaire/cours", icon: BookOpen },
-  { label: "Inscription Documents", path: "/secretaire/documents", icon: ClipboardList },
   { label: "Notes", path: "/secretaire/notes", icon: FileCheck },
   { label: "Paiements", path: "/secretaire/paiements", icon: CreditCard },
+  { label: "Inscription Documents", path: "/secretaire/documents", icon: ClipboardList },
+  
+  
 ];
--- frontend/src/routes/secretaryRoutes.jsx
+++ frontend/src/routes/secretaryRoutes.jsx
@@ -4,14 +4,15 @@ import Notes from '../pages/secretary/Notes';
 import Controles from "../pages/secretary/Controles";
 import Cours from "../pages/secretary/Cours.jsx";
 import Paiements from "../pages/secretary/paiements.jsx";
-
+import InscriptionDocuments from "../pages/secretary/InscriptionDocuments.jsx";
 const secretaryRoutes = [
     { path: "/secretaire/stagiaire", element: <StagiaireDashboard /> },
     { path: "/secretaire/stagiaire/ajouterStagiaire", element: <AjouterStagiaire /> },
     { path: "/secretaire/notes", element: <Notes /> },
     { path: "/secretaire/controles", element: <Controles /> },
     { path: "/secretaire/cours", element: <Cours /> },
-    { path: "/secretaire/paiements", element: <Paiements /> }
+    { path: "/secretaire/paiements", element: <Paiements /> },
+    { path: "/secretaire/documents", element: <InscriptionDocuments /> }
 
     
 
