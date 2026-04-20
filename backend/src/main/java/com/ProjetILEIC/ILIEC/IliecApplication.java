6f536f5423581363d8ddb94bda274a3852acc746
ayoub <ayoubpro183@gmail.com>
2026-04-20 21:52:01 +0100

    feat: add spring boot backend folder

---
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/IliecApplication.java
@@ -0,0 +1,13 @@
+package com.ProjetILEIC.ILIEC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IliecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IliecApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ProjetILEIC/ILIEC/IliecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProjetILEIC.ILIEC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IliecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
