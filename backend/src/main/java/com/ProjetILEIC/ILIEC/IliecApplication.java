817cdeaf04b348759b402031c54cfbd8fc0cf1c0
ayoub <ayoubpro183@gmail.com>
2026-04-22 16:44:27 +0100

    added docker files

--- backend/src/main/java/com/ProjetILEIC/ILIEC/IliecApplication.java
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/IliecApplication.java
@@ -2,10 +2,8 @@ package com.ProjetILEIC.ILIEC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-
+@SpringBootApplication
 public class IliecApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IliecApplication.class, args);
