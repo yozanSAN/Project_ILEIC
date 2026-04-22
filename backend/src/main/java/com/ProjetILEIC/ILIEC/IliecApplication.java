203a93773c8934259b6d814cfb16f0aa2fae14da
ayoub <ayoubpro183@gmail.com>
2026-04-22 15:53:41 +0100

    stop tracking application.properties

--- backend/src/main/java/com/ProjetILEIC/ILIEC/IliecApplication.java
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/IliecApplication.java
@@ -2,12 +2,12 @@ package com.ProjetILEIC.ILIEC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+
 public class IliecApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(IliecApplication.class, args);
 	}
-
 }
