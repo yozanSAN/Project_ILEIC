6d1a25a3eea3116f5c23b0660f99fe758324eca1
ayoub <ayoubpro183@gmail.com>
2026-06-10 22:42:50 +0100

    Changing the return value from a string to a clean responseDTO

--- backend/src/main/java/com/ProjetILEIC/ILIEC/controller/AuthController.java
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/controller/AuthController.java
@@ -1,9 +1,13 @@
 package com.ProjetILEIC.ILIEC.controller;
 
+import com.ProjetILEIC.ILIEC.dto.AuthResponseDTO;
+import com.ProjetILEIC.ILIEC.dto.LoginRequestDTO;
 import com.ProjetILEIC.ILIEC.entity.User;
 import com.ProjetILEIC.ILIEC.repository.UserRepository;
 import com.ProjetILEIC.ILIEC.security.JwtUtil;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -13,6 +17,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @AllArgsConstructor
 @RestController
@@ -25,24 +30,29 @@ public class AuthController {
     private final JwtUtil jwtUtil;
 
     @PostMapping("/login")
-    public String authenticateUser(@RequestBody User user) {
+    public ResponseEntity<?> authenticateUser(@RequestBody LoginRequestDTO requestDTO) {
         try {
             Authentication authentication = authenticationManager.authenticate(
                 new UsernamePasswordAuthenticationToken(
-                        user.getEmail(),
-                        user.getPasswordHash()
+                        requestDTO.getEmail(),
+                        requestDTO.getPassword()
                 )
             );
-         // 2. Extract user details from the authentication principal
+            // 2. Extract user details from the authentication principal
             UserDetails userDetails = (UserDetails) authentication.getPrincipal();
 
             // 3. Generate the token string
             String token = jwtUtil.generateToken(userDetails.getUsername());
 
-             // 4. Return the raw token back as plain text with user emial
-            return "Token: " + token + " | Email: " + userDetails.getUsername();
+            //4.Find the user by email and return an exception if not found
+            User user = userRepository.findByEmail(userDetails.getUsername())
+                    .orElseThrow(() -> new RuntimeException("User not Found!!"));
+
+             // 4. Return a clean userDTO object instead of a string
+            return ResponseEntity.ok(new AuthResponseDTO(token, user.getEmail() ,user.getRole()));
         } catch (Exception e) {
-            return "Authentication failed: Invalid email or school code." + e + e.getMessage();
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body("Authentication failed: invalid email or password");
         }
     }
 
