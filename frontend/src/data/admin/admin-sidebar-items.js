f0ce58635ea071dfeed0c260331094de3c1c63e5
ayoub <ayoubpro183@gmail.com>
2026-06-13 16:23:52 +0100

    fixing the missing / in paths

--- frontend/src/data/admin/admin-sidebar-items.js
+++ frontend/src/data/admin/admin-sidebar-items.js
@@ -2,7 +2,7 @@ import {LayoutDashboard,Users,UserCheck,GraduationCap,} from "lucide-react";
   
   export const adminItems = [
     {label: "Dashboard",path: "/admin",icon: LayoutDashboard,},
-    {label: "Formateurs",path: "formateur/personnel-detail",icon: UserCheck,},
-    {label: "Stagiaires",path: "stagiaire/profil",icon: GraduationCap,},
-    {label: "Secrétaires",path: "secretaire/stagiaire",icon: Users,},
+    {label: "Formateurs",path: "/formateur/personnel-detail",icon: UserCheck,},
+    {label: "Stagiaires",path: "/stagiaire/profil",icon: GraduationCap,},
+    {label: "Secrétaires",path: "/secretaire/stagiaire",icon: Users,},
   ];
