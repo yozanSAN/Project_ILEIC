e69ab4c0a99e2459e74d6d74f8687eab5e74b564
ayoub <ayoubpro183@gmail.com>
2026-06-18 15:10:32 +0100

    feat: including the missing fields in PaymentDTO

--- backend/src/main/java/com/ProjetILEIC/ILIEC/dto/PaymentDTO.java
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/dto/PaymentDTO.java
@@ -3,20 +3,23 @@ package com.ProjetILEIC.ILIEC.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
 public class PaymentDTO {
     private Long id;
     private Long stagiaireId;
-    private String stagiaireName;
+    private String stagiaireFullName;
     private Integer month;
     private Integer year;
     private BigDecimal amount;
-    private String status;
+    private LocalDate paymentDate;
     private String paymentMethod;
-    private String recordedByEmail;
+    private String status;
+    private String reference;
+    private Long recordedById;
+    private String recordedByFullName;
 }
