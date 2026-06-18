ef33f0f2ccda10cbfd24063ca3e5680a97a93bb9
ayoub <ayoubpro183@gmail.com>
2026-06-18 23:20:11 +0100

    feat: added the missing FormateurRequestDTO class

--- backend/src/main/java/com/ProjetILEIC/ILIEC/dto/FormateurRequestDTO.java
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/dto/FormateurRequestDTO.java
@@ -1,4 +1,17 @@
 package com.ProjetILEIC.ILIEC.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FormateurRequestDTO {
+    private Long userId;
+    private String speciality;
+    private LocalDate hireDate;
+    private List<Long> centreIds;
 }
