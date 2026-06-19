2456532152b0283d770278caf56fc0c5ca1138aa
ayoub <ayoubpro183@gmail.com>
2026-06-19 16:49:31 +0100

    feat(CentreRequestDTO): removed the validation annotations(will add all validations later)

--- backend/src/main/java/com/ProjetILEIC/ILIEC/dto/CentreRequestDTO.java
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/dto/CentreRequestDTO.java
@@ -9,18 +9,8 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CentreRequestDTO {
-
-    @NotBlank(message = "Centre name is required")
-    @Size(max = 150, message = "Name must not exceed 150 characters")
     private String name;
-
-    @Size(max = 255, message = "Address must not exceed 255 characters")
     private String address;
-
-    @Size(max = 30, message = "Phone must not exceed 30 characters")
     private String phone;
-
-    @Email(message = "Please provide a valid email address")
-    @Size(max = 150, message = "Email must not exceed 150 characters")
     private String email;
 }
