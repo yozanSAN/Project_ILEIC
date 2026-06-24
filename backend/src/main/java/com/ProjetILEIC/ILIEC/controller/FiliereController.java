0a718650aceed72f3501652e6ba958ea44eac9d6
ayoub <ayoubpro183@gmail.com>
2026-06-24 14:43:47 +0100

    feat(filiere): created GET and GET by id endpoints,tested and working

--- backend/src/main/java/com/ProjetILEIC/ILIEC/controller/FiliereController.java
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/controller/FiliereController.java
@@ -1,4 +1,32 @@
 package com.ProjetILEIC.ILIEC.controller;
 
+import com.ProjetILEIC.ILIEC.dto.FiliereDTO;
+import com.ProjetILEIC.ILIEC.service.FiliereService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/filieres")
 public class FiliereController {
+    private final FiliereService filiereService;
+
+    public FiliereController(FiliereService filiereService) {
+        this.filiereService = filiereService;
+    }
+    // GET ALL ACADIMEC FILIERES
+    @GetMapping
+    public ResponseEntity<List<FiliereDTO>> getAllFilieres(){
+        return ResponseEntity.ok(filiereService.getAllFilieres());
+    }
+
+    //RETRIEVE DETAILED INFORMATIONS FOR A SINGLE SPECIFIC FILIERE
+    @GetMapping("/{id}")
+    public ResponseEntity<FiliereDTO> getFilieById(@PathVariable Long id){
+        return ResponseEntity.ok(filiereService.getFiliereById(id));
+    }
 }
