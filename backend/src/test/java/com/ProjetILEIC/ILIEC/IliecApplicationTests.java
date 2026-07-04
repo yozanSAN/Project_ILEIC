e58f20b55df0e2e81bcad2c155089e4248ac9e6f
ayoub <ayoubpro183@gmail.com>
2026-07-04 15:20:58 +0100

    fix(secretary): added one-secretary-per-centre check inside createSecretary() and updateSecretary() in SecretaryService

--- backend/src/test/java/com/ProjetILEIC/ILIEC/IliecApplicationTests.java
+++ backend/src/test/java/com/ProjetILEIC/ILIEC/IliecApplicationTests.java
@@ -3,11 +3,10 @@ package com.ProjetILEIC.ILIEC;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class IliecApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
