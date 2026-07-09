ae6b4aae68f4f3585d9e9c57515285d8da953c32
ayoub <ayoubpro183@gmail.com>
2026-07-09 17:14:22 +0100

    feat(refreshToken): created the RefreshToken entity

---
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/entity/RefreshToken.java
@@ -0,0 +1,29 @@
+package com.ProjetILEIC.ILIEC.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.time.Instant;
+
+@Entity
+@Table(name = "refresh_tokens")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RefreshToken {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String token;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @Column(nullable = false)
+    private Instant expiresAt;
+}
