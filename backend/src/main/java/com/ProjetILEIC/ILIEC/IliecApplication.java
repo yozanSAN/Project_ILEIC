d799d090bf1b64033bec2624cf796552f8aa33b5
ayoub <ayoubpro183@gmail.com>
2026-07-15 17:58:57 +0100

    chore(audit): Remove old manual audit logging and configure Hibernate Envers dependencies

--- backend/src/main/java/com/ProjetILEIC/ILIEC/IliecApplication.java
+++ backend/src/main/java/com/ProjetILEIC/ILIEC/IliecApplication.java
@@ -2,7 +2,11 @@ package com.ProjetILEIC.ILIEC;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
 public class IliecApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(IliecApplication.class, args);
